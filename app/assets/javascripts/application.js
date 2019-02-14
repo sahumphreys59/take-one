dd0e57e5958d08db89f86e71fb7e96299ea26b78
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-02-14 17:59:54 +0000

    Trying to figure out font in splurty

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
+//= require_tree .
